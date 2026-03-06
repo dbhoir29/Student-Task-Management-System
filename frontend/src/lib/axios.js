87c5479f5339dd90c70896f878cbb80f0d5a536d
dbhoir29 <chmcs2026.devashish.bhoir@gmail.com>
2026-03-06 15:09:49 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://student-task-management-system-backend.onrender.com/tasks",
+    baseURL: "https://student-task-management-system-backend.onrender.com/tasks/summary",
 });
 
 export default api;
