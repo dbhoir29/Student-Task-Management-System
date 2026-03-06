a08254c1205004d4de0193312f97cd4e567fe035
dbhoir29 <chmcs2026.devashish.bhoir@gmail.com>
2026-03-06 15:12:38 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-    //origin: "http://localhost:5173",
+    origin: "https://student-task-management-system-frontend.onrender.com/",
 }));
 
 app.use(express.json());
